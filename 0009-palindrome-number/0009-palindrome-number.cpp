939de118b8929fa9f04a817ef6c19dcb00da7c92
Shaik Irfan <shaikirfan3771@gmail.com>
2026-08-18 13:11:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
+        if(x<0) return false;
         int temp=x;
         long long comp=0;
         while(temp!=0){
